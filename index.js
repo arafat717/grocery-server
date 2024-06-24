81226f99eaed6fffb3dff91858e8e78a75b11b6c
arafat717 <arafatjibon33@gmail.com>
2024-06-24 09:33:56 +0600

    added cart get method

--- index.js
+++ index.js
@@ -59,6 +59,16 @@ async function run() {
 
     // all carts api here ////
 
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const cart = req.body;
       const result = await cartCollection.insertOne(cart);
